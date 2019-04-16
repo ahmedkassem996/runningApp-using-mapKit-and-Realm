7721698d5930b74cc0b3953c92066c730a4f58c8
AHMED <ahmed@AHMEDs-MacBook-Pro.local>
2019-04-16 17:12:12 +0200

    Initial Commit

---
+++ Treads/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Treads
+//
+//  Created by AHMED on 1/27/1398 AP.
+//  Copyright © 1398 AHMED. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
